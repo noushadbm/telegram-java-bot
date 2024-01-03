f90d420a5b0c5761bb2c77f3a8375fe18cb53177
noushadbm <noushadbm@gmail.com>
2024-01-04 00:04:22 +0400

    Initial commit for java-bot

---
+++ src/main/java/com/rayshan/bot/SpringBootConsoleApplication.java
@@ -0,0 +1,22 @@
+package com.rayshan.taskscheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsoleApplication implements CommandLineRunner {
+	private static Logger LOG = LoggerFactory
+			.getLogger(SpringBootConsoleApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConsoleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("EXECUTING : command line runner");
+	}
+}
